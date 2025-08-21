8e8b710538ea26780ad67cfee027e0f0079a24cd
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-08-21 17:55:09 +0000

    creacion de clase Libro

---
+++ miPrincipal/Libro.java
@@ -0,0 +1,38 @@
+package miPrincipal;
+public class Libro{
+    //Atributos
+    private String titulo;
+    private String autor;
+    private int anio;
+    //contructores
+    public Libro(){
+        this.titulo = "No Definido";
+        this.autor = "No Definido";
+        this.anio = 0;
+    }
+    public Libro(String titulo, String autor,int anio){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+    }
+    //getter y setter
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+    public int getAnio(){
+        return anio;
+    }
+    public void setAnio(int anio){
+        this.anio = anio;
+    }
+
+}
