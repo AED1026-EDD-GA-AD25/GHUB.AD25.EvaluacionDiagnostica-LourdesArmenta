156f715ecadd04d568ca0fbae90a8094fdfebec0
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-08-26 17:18:05 +0000

    solicion final

--- miPrincipal/InventarioLibros.java
+++ miPrincipal/InventarioLibros.java
@@ -1,20 +1,51 @@
 package miPrincipal;
 public class InventarioLibros{
+    Libro listaLibros[]; //creando un arreglo de libros
+    int contador;
     //constructores
+    public InventarioLibros(){
+        listaLibros = new Libro[2];
+        contador = 0;
+    }
     //metodos
     //agrega libros a la lista
     public void agregar(Libro libro){
+        //agregar a la lista
+        if(contador<listaLibros.length){
+            listaLibros[contador]= libro;
+            contador++;
+        }else{
+            //la lista esta llena, por lo tanto hay que aumentarla
+            Libro listaTemp[] = new Libro[listaLibros.length*2];
+            //copia la lista original
+            for(int i=0;i<listaLibros.length;i++){
+                listaTemp[i]=listaLibros[i];
+            }
+            listaLibros = listaTemp; 
+            listaLibros[contador]= libro;
+            contador++;
+        }
 
     }
     //busca en la lista a un libro por autor
     //si lo encuentra retorna el libro
     //sino retorna null
     public Libro buscar(String autor){
+        for (int i=0;i<contador;i++){
+            if(listaLibros[i].getAutor().equalsIgnoreCase(autor)){
+                return listaLibros[i];
+            }
+        }
+        //si llega a este lugar, es porque el libros
+        //con el autor deseado no existe
         return null;
 
     }
     //mostrar por pantalla toda lista de libros
     public void mostrar(){
+        for(int i=0;i<contador;i++){
+            System.out.println(listaLibros[i]);
+        }
 
     }
 
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,9 +1,61 @@
 package miPrincipal;
+import java.util.Scanner;
+
 
 public class Principal {
-    
-
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        int opc=0;
+        String autor,titulo;
+        int anio;
+        InventarioLibros inventarioLibros = new InventarioLibros();
+        do{
+            menu();
+            opc = sc.nextInt();
+            sc.nextLine();//consume el enter
+            switch(opc){
+                case 0:
+                    System.out.println("Hasta luego!");
+                    break;
+                case 1:
+                    System.out.print("Proporciona nombre de autor:");
+                    autor = sc.nextLine();
+                    System.out.print("Proporciona titulo:");
+                    titulo = sc.nextLine();
+                    System.out.print("Proporciona año de publicacion:");
+                    anio= sc.nextInt();
+                    Libro libro = new Libro(titulo,autor,anio);
+                    inventarioLibros.agregar(libro);
+
+                    break;
+                case 2:
+                    System.out.print("Proporciona autor del libro:");
+                    autor = sc.nextLine();
+                    Libro libroBuscado;
+                    libroBuscado = inventarioLibros.buscar(autor);
+                    if (libroBuscado != null)
+                        System.out.println("Libro Encontrado: "+libroBuscado);
+                    else
+                        System.out.println("Libro No encontrado!!");
+                    break;
+
+                case 3:
+                    inventarioLibros.mostrar();
+                    break;
+                default:
+
+            }
+            
+        }while (opc != 0);
        
+    }
+    public static void menu(){
+        System.out.println("Menu Principal");
+        System.out.println("1. Agregar");
+        System.out.println("2. Buscar por autor");
+        System.out.println("3. Mostrar todos");
+        System.out.println();
+        System.out.println("0. Salir");
+        System.out.print("Seleccione opción:");
     }
 }
